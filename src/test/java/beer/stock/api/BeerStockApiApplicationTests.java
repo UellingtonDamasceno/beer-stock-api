276940fd37ab7a9dacb5f47c690dde1de1c1b508
Uellington Damasceno <uellington99@gmail.com>
2021-03-13 01:49:14 -0300

    Project created with spring initializr.

---
+++ src/test/java/beer/stock/api/BeerStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package beer.stock.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
